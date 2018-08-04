595013dde69d09e4a0497c72636e11e65e577449
jkarpen <jsh.karpen@gmail.com>
2018-08-04 14:23:31 -0700

    Adding rest of chapter 2.1
    Finished code examples in chapter 2.1 of Deep Learning Book.

--- Linear Algebra - Deep Learning Book/Chapter 2_1 - Scalars_Vectors_Matrices_and_Tensors.py
+++ Linear Algebra - Deep Learning Book/Chapter 2_1 - Scalars_Vectors_Matrices_and_Tensors.py
@@ -13,25 +13,61 @@ https://hadrienj.github.io/posts/Deep-Learning-Book-Series-2.1-Scalars-Vectors-M
 import numpy as np
 
 ##### Vector
-#Start by creating a vector, a 1-dimensional array
+# Start by creating a vector, a 1-dimensional array
 x = np.array([1, 2, 3, 4])
 
 ##### Matrix
-#Create a 3x2 matrix
-#Array() can create 2-dimensional arrays with nested brackets
+# Create a 3x2 matrix
+# Array() can create 2-dimensional arrays with nested brackets
 A = np.array([[1, 2],
               [3, 4],
               [5, 6]])
 
 ##### Shape
-#Find the shape of the array
+# Find the shape of the array
 A.shape
-#Returns (3, 2), telling us the number of rows and columns
+# Returns (3, 2), telling us the number of rows and columns
 
-#Check the shape of the first vector we created
+# Check the shape of the first vector we created
 x.shape
-#Returns (4, ), because it has 4 rows and no columns
-#This matches the length of the array
+# Returns (4, ), because it has 4 rows and no columns
+# This matches the length of the array
 len(x) #returns 4
 
 ##### Transposition
+ 
+# Create matrix A
+A = np.array([[1, 2], [3, 4], [5, 6]])
+# Transpose it
+A_t = A.T
+
+A # View original
+A_t # View transposed version
+
+# Use shape to view dimensions of these matrices
+A.shape
+A_t.shape
+
+
+##### Addition
+
+#Create matrices A and B
+A = np.array([[1, 2], [3, 4], [5, 6]])
+B = np.array([[2, 5], [7, 4], [4, 3]])
+
+C = A + B
+C
+
+# Can also add a scalar to a matrix
+C = C + 1
+
+##### Broadcasting
+A = np.array([[1, 2], [3, 4], [5, 6]])
+B = np.array([[2], [4], [6]])
+
+C = A+B
+C
+
+
+
+
